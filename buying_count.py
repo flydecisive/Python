ae8f70c6198c6d718c66e41af3201d8b786e34c1
Flydecisive <flydecisive@yandex.ru>
2022-03-03 17:14:10 +0300

    Stepik

---
+++ buying_count.py
@@ -0,0 +1,8 @@
+computers_count = 3
+monitor_coast = int(input())
+system_block_coast = int(input())
+keyboard_coast = int(input())
+mouse_coast = int(input())
+
+total_coast = computers_count * (monitor_coast + system_block_coast + keyboard_coast + mouse_coast)
+print(total_coast)
