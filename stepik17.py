a173349469716d68240d22bc3f53c8c72741ab13
Flydecisive <flydecisive@yandex.ru>
2022-03-05 18:35:02 +0300

    stepik

---
+++ stepik17.py
@@ -0,0 +1,14 @@
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+
+sum = 0
+
+if num_1 > 0:
+    sum += num_1
+if num_2 > 0:
+    sum += num_2
+if num_3 > 0:
+    sum += num_3
+
+print(sum)
