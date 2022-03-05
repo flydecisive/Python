7c8537dc49a128493cfe507e28c8026d3e78a421
Flydecisive <flydecisive@yandex.ru>
2022-03-05 17:11:50 +0300

    Stepik

---
+++ stepik13.py
@@ -0,0 +1,11 @@
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+
+diff_1 = num_2 - num_1
+diff_2 = num_3 - num_2
+
+if diff_1 == 1 and diff_2 == 1:
+    print('YES')
+else:
+    print('NO')
