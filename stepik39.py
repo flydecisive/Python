d35aa47d811c6087e16b43456753db8e145c5fe1
Flydecisive <flydecisive@yandex.ru>
2022-03-07 17:28:16 +0300

    'stepik'

---
+++ stepik39.py
@@ -0,0 +1,8 @@
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+num_5 = int(input())
+
+print(f'Наименьшее число = {min(num_1, num_2, num_3, num_4, num_5)}')
+print(f'Наибольшее число = {max(num_1, num_2, num_3, num_4, num_5)}')
