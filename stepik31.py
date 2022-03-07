540ed2414c92c49bc9c77e922f4d2e729629e37c
Flydecisive <flydecisive@yandex.ru>
2022-03-07 15:33:30 +0300

    'stepik'

--- stepik31.py
+++ stepik31.py
@@ -5,17 +5,30 @@ b_2 = int(input())
 
 if a_1 > b_2 or b_1 < a_2:
     print('пустое множество')
+elif a_1 == b_2:
+    print(f'{a_1}')
+elif a_2 == b_1:
+    print(f'{a_2}')
 elif a_1 < a_2:
     if b_1 < b_2:
         print(f'{a_2} {b_1}')
     elif b_1 > b_2:
         print(f'{a_2} {b_2}')
     else:
-
+        print(f'{a_2} {b_2}')
 elif a_1 > a_2:
-
+    if b_1 > b_2:
+        print(f'{a_1} {b_2}')
+    elif b_1 < b_2:
+        print(f'{a_1} {b_1}')
+    else:
+        print(f'{a_1} {b_1}')
 elif a_1 == a_2:
+    if b_1 < b_2:
+        print(f'{a_1} {b_1}')
+    elif b_1 > b_2:
+        print(f'{a_1} {b_2}')
+    else:
+        print(f'{a_1} {b_1}')
 
-elif a_1 == b_2:
 
-elif a_2 == b_1:
