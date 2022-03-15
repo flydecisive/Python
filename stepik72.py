25146a1790e5883e5162cebc8b15f562ff7e929f
Flydecisive <flydecisive@yandex.ru>
2022-03-15 23:07:53 +0300

    'stepik'

---
+++ stepik72.py
@@ -0,0 +1,17 @@
+cell_x_1 = int(input())
+cell_y_1 = int(input())
+cell_x_2 = int(input())
+cell_y_2 = int(input())
+
+sum_cell_1 = cell_x_1 + cell_y_1
+sum_cell_2 = cell_x_2 + cell_y_2
+
+dif_1 = abs(cell_x_1 - cell_x_2)
+dif_2 = abs(cell_y_1 - cell_y_2)
+
+if (sum_cell_1 % 2 == 0 and sum_cell_2 % 2 == 0 and dif_1 == dif_2) or (cell_x_1 == cell_x_2 and (cell_y_1 >= cell_y_2 or cell_y_1 <= cell_y_2)):
+    print('YES')
+elif (sum_cell_1 % 2 != 0 and sum_cell_2 % 2 != 0 and dif_1 == dif_2) or (cell_y_1 == cell_y_2 and (cell_x_1 >= cell_x_2 or cell_x_1 <= cell_x_2)):
+    print('YES')
+else:
+    print('NO')
